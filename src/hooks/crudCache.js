28f417e26d0870aaf012ba5dcbd0c4380c48ca51
DaddyWarbucks <beau@pxlpotion.com>
2020-02-24 22:48:56 -0600

    Move the get, set, delete logic to custom cache map

--- src/hooks/crudCache.js
+++ src/hooks/crudCache.js
@@ -6,18 +6,6 @@ const defaultMakeCacheKey = (key, context) => {
   return key.toString ? key.toString() : key;
 };
 
-const defaultShouldSet = (result, context) => {
-  const { query = {} } = context.params || {};
-  return !query.$select;
-};
-
-const defaultShouldGet = context => {
-  const { query = {} } = context.params || {};
-  return !Object.keys(query).length;
-};
-
-const defaultShouldDelete = () => true;
-
 const defaultGetResultKey = (result, context) => {
   const keyField = (context.service || {}).id || 'id';
   return result[keyField];
@@ -27,13 +15,10 @@ module.exports = function(cacheMap, options = {}) {
   const clone = options.clone || defaultClone;
   const makeCacheKey = options.makeCacheKey || defaultMakeCacheKey;
   const getResultKey = options.getResultKey || defaultGetResultKey;
-  const shouldSet = options.shouldSet || defaultShouldSet;
-  const shouldGet = options.shouldGet || defaultShouldGet;
-  const shouldDelete = options.shouldDelete || defaultShouldDelete;
 
   return skippable('crudCache', async context => {
     if (context.type === 'before') {
-      if (context.method === 'get' && (await shouldGet(context))) {
+      if (context.method === 'get') {
         const key = await makeCacheKey(context.id, context);
         const value = await cacheMap.get(key, context);
         if (value) {
@@ -47,24 +32,20 @@ module.exports = function(cacheMap, options = {}) {
       if (context.method === 'remove') {
         await Promise.all(
           results.map(async result => {
-            if (await shouldDelete(result, context)) {
-              const resultKey = await getResultKey(result, context);
-              const key = await makeCacheKey(resultKey, context);
-              const cloned = await clone(result, context);
-              return cacheMap.delete(key, cloned, context);
-            }
+            const resultKey = await getResultKey(result, context);
+            const key = await makeCacheKey(resultKey, context);
+            const cloned = await clone(result, context);
+            return cacheMap.delete(key, cloned, context);
           })
         );
         return context;
       } else {
         await Promise.all(
           results.map(async result => {
-            if (await shouldSet(result, context)) {
-              const resultKey = await getResultKey(result, context);
-              const key = await makeCacheKey(resultKey, context);
-              const cloned = await clone(result, context);
-              return cacheMap.set(key, cloned, context);
-            }
+            const resultKey = await getResultKey(result, context);
+            const key = await makeCacheKey(resultKey, context);
+            const cloned = await clone(result, context);
+            return cacheMap.set(key, cloned, context);
           })
         );
         return context;
--- tests/hooks/crudCache.test.js
+++ tests/hooks/crudCache.test.js
@@ -1,5 +1,6 @@
 const assert = require('assert');
 const crudCache = require('../../src/hooks/crudCache');
+const CrudCacheMap = require('../../src/lib/crudCacheMap');
 
 describe('crudCache', () => {
   const result1 = { id: 1, title: 'The Man in Black' };
@@ -12,12 +13,12 @@ describe('crudCache', () => {
       id: 1
     };
 
-    const cacheMap = new Map();
-    cacheMap.set('1', result1);
+    const cacheMap = new CrudCacheMap();
+    cacheMap.set('1', result1, context);
 
     const newContext = await crudCache(cacheMap)(context);
 
-    await assert.deepEqual(cacheMap.get('1'), newContext.result);
+    await assert.deepEqual(cacheMap.get('1', context), newContext.result);
   });
 
   it('Does not return from the cache on get() if querying', async () => {
@@ -28,8 +29,8 @@ describe('crudCache', () => {
       params: { query: { title: 'The Man in Black' } }
     };
 
-    const cacheMap = new Map();
-    cacheMap.set('1', result1);
+    const cacheMap = new CrudCacheMap();
+    cacheMap.set('1', result1, context);
 
     const newContext = await crudCache(cacheMap)(context);
 
@@ -43,12 +44,12 @@ describe('crudCache', () => {
       result: { data: [result1, result2] }
     };
 
-    const cacheMap = new Map();
+    const cacheMap = new CrudCacheMap();
 
     const newContext = await crudCache(cacheMap)(context);
 
-    await assert.deepEqual(cacheMap.get('1'), result1);
-    await assert.deepEqual(cacheMap.get('2'), result2);
+    await assert.deepEqual(cacheMap.get('1', context), result1);
+    await assert.deepEqual(cacheMap.get('2', context), result2);
   });
 
   it('Caches records on get()', async () => {
@@ -58,11 +59,11 @@ describe('crudCache', () => {
       result: result1
     };
 
-    const cacheMap = new Map();
+    const cacheMap = new CrudCacheMap();
 
     const newContext = await crudCache(cacheMap)(context);
 
-    await assert.deepEqual(cacheMap.get('1'), result1);
+    await assert.deepEqual(cacheMap.get('1', context), result1);
   });
 
   it('Caches records on create()', async () => {
@@ -72,11 +73,11 @@ describe('crudCache', () => {
       result: result1
     };
 
-    const cacheMap = new Map();
+    const cacheMap = new CrudCacheMap();
 
     const newContext = await crudCache(cacheMap)(context);
 
-    await assert.deepEqual(cacheMap.get('1'), result1);
+    await assert.deepEqual(cacheMap.get('1', context), result1);
   });
 
   it('Caches records on update()', async () => {
@@ -86,11 +87,11 @@ describe('crudCache', () => {
       result: result1
     };
 
-    const cacheMap = new Map();
+    const cacheMap = new CrudCacheMap();
 
     const newContext = await crudCache(cacheMap)(context);
 
-    await assert.deepEqual(cacheMap.get('1'), result1);
+    await assert.deepEqual(cacheMap.get('1', context), result1);
   });
 
   it('Caches records on patch()', async () => {
@@ -100,11 +101,11 @@ describe('crudCache', () => {
       result: result1
     };
 
-    const cacheMap = new Map();
+    const cacheMap = new CrudCacheMap();
 
     const newContext = await crudCache(cacheMap)(context);
 
-    await assert.deepEqual(cacheMap.get('1'), result1);
+    await assert.deepEqual(cacheMap.get('1', context), result1);
   });
 
   it('Removes records on remove()', async () => {
@@ -114,25 +115,26 @@ describe('crudCache', () => {
       result: result1
     };
 
-    const cacheMap = new Map();
+    const cacheMap = new CrudCacheMap();
 
     const newContext = await crudCache(cacheMap)(context);
 
-    await assert.deepEqual(cacheMap.get('1'), undefined);
+    await assert.deepEqual(cacheMap.get('1', context), undefined);
   });
 
   it('Does not cache records when filtering', async () => {
     const context = {
       type: 'after',
       method: 'find',
-      params: { query: { $select: ['title'] } }
+      params: { query: { $select: ['title'] } },
+      result: [result1]
     };
 
-    const cacheMap = new Map();
+    const cacheMap = new CrudCacheMap();
 
     const newContext = await crudCache(cacheMap)(context);
 
-    await assert.deepEqual(cacheMap.get('1'), undefined);
+    await assert.deepEqual(cacheMap.get('1', context), undefined);
   });
 
   it('Can use a custom getResultKey option', async () => {
@@ -142,13 +144,13 @@ describe('crudCache', () => {
       result: [result1]
     };
 
-    const cacheMap = new Map();
+    const cacheMap = new CrudCacheMap();
 
     const getResultKey = result => result.id;
 
     const newContext = await crudCache(cacheMap, { getResultKey })(context);
 
-    await assert.deepEqual(cacheMap.get('1'), result1);
+    await assert.deepEqual(cacheMap.get('1', cacheMap), result1);
   });
 
   it('Can use a custom makeCacheKey option', async () => {
@@ -158,64 +160,13 @@ describe('crudCache', () => {
       result: [result1]
     };
 
-    const cacheMap = new Map();
+    const cacheMap = new CrudCacheMap();
 
     const makeCacheKey = key => 'myKey';
 
     const newContext = await crudCache(cacheMap, { makeCacheKey })(context);
 
-    await assert.deepEqual(cacheMap.get('myKey'), result1);
-  });
-
-  it('Can use a custom shouldSet option', async () => {
-    const context = {
-      type: 'after',
-      method: 'find',
-      result: [result1]
-    };
-
-    const cacheMap = new Map();
-
-    const shouldSet = (result, context) => false;
-
-    const newContext = await crudCache(cacheMap, { shouldSet })(context);
-
-    await assert.deepEqual(cacheMap.get('1'), undefined);
-  });
-
-  it('Can use a custom shouldGet option', async () => {
-    const context = {
-      type: 'before',
-      method: 'get',
-      id: 1
-    };
-
-    const cacheMap = new Map();
-    cacheMap.set('1', result1);
-
-    const shouldGet = context => false;
-
-    const newContext = await crudCache(cacheMap, { shouldGet })(context);
-
-    await assert.deepEqual(context.result, undefined);
-  });
-
-  it('Can use a custom shouldDelete option', async () => {
-    const context = {
-      type: 'after',
-      method: 'remove',
-      id: 1,
-      result: result1
-    };
-
-    const cacheMap = new Map();
-    cacheMap.set('1', result1);
-
-    const shouldDelete = context => false;
-
-    const newContext = await crudCache(cacheMap, { shouldDelete })(context);
-
-    await assert.deepEqual(cacheMap.get('1'), result1);
+    await assert.deepEqual(cacheMap.get('myKey', cacheMap), result1);
   });
 
   it('Can handle an async cacheMap', async () => {
@@ -232,10 +183,10 @@ describe('crudCache', () => {
       delete: async key => map.delete(key)
     };
 
-    await cacheMap.set('1', result1);
+    await cacheMap.set('1', result1, context);
 
     const newContext = await crudCache(cacheMap)(context);
 
-    await assert.deepEqual(await cacheMap.get('1'), context.result);
+    await assert.deepEqual(await cacheMap.get('1', context), context.result);
   });
 });
