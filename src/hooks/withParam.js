767b53a4adb1947c212b867f50107b1b01eb3d1a
rubyrubenstahl <ruby@krohtech.com>
2021-09-24 00:04:28 -0400

    Copy the withData hook and test files to withParam

---
+++ src/hooks/withParam.js
@@ -0,0 +1,25 @@
+const { skippable } = require('../lib');
+const { virtualsSerializer, resolver } = require('../lib/virtualsSerializer');
+
+// Add data, such as defaults to context.data in a before hook.
+// Note `data` could technically be an array of multiple items
+// to create/update/patch. Also note that although the keys are
+// iterated over syncronously (in order of definition on the virtuals
+// object) that if data is an array, all items in the data array
+// are run in parrallel.
+// The value of each property in the virtuals object can be a function,
+// a promise, a function that returns a promise, or a simple value. The
+// virtual should return the value to be attached to the key and should
+// not mutate context directly.
+module.exports = (virtuals, prepFunc = () => {}) => {
+  return skippable('withData', async context => {
+    context.data = await virtualsSerializer(
+      resolver,
+      context.data,
+      virtuals,
+      context,
+      prepFunc
+    );
+    return context;
+  });
+};
