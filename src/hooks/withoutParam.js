cc7693eac65a1be9446a3c0eb1d0064d93ff6d73
rubyrubenstahl <ruby@krohtech.com>
2021-09-24 02:17:35 -0400

    implement withoutParam.js

---
+++ src/hooks/withoutParam.js
@@ -0,0 +1,32 @@
+const unset = require('unset-value');
+
+const { skippable } = require("../lib");
+const {
+  virtualsSerializer,
+  filterResolver,
+} = require("../lib/virtualsSerializer");
+const { omit } = require("../lib/utils");
+
+module.exports = (virtuals, prepFunc = () => {}) => {
+  return skippable("withoutParam", async (context) => {
+    if (!context.params) {
+      context.params = {};
+    }
+
+    if (Array.isArray(virtuals)) {
+      context.data = Array.isArray(context.data)
+        ? context.data.map(d => unset(context.params, virtuals))
+        : unset(context.params, virtuals);
+      return context;
+    } 
+
+    context.params = await virtualsSerializer(
+      filterResolver,
+      context.params,
+      virtuals,
+      context,
+      prepFunc
+    );
+    return context;
+  });
+};
