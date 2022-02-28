25155a05acd770e55e89d45c1f8facc26b997576
DaddyWarbucks <techtbeau@gmail.com>
2022-02-28 11:29:02 -0700

    Update URL

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy docs'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:daddywarbucks/feathers-fletching.git master:gh-pages
+git push -f git@github.com:DaddyWarbucks/feathers-fletching.git master:gh-pages
 
 cd -
