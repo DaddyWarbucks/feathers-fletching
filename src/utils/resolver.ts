e0cc437ae31e703f9f9de00b5841529d3adf3838
DaddyWarbucks <techtbeau@gmail.com>
2024-06-09 18:55:33 -0500

    Add resolver

--- src/utils/resolver.ts
+++ src/utils/resolver.ts
@@ -1,59 +1,119 @@
 import type { HookContext } from '@feathersjs/feathers';
-import type { MaybeArray, Promisable } from './utils';
+import { GeneralError } from '@feathersjs/errors';
+import toposort from "toposort";
 import { isEmpty, isPromise } from './utils';
-
-// type ResolverFn = typeof resolver;
 
 export type ResolverFunction = (
   data: Record<string, any>,
   context: HookContext,
-  resolvers: Record<string, any>
+  resolver: PropertyResolver
 ) => any;
 
 export type ResolverFunctions = Record<string, ResolverFunction>;
 
-interface Resolver {
+export class Resolver {
   options: {
     resolvers: ResolverFunctions;
   };
-}
 
-class Resolver {
   constructor(resolvers: ResolverFunctions) {
-    // this.data = data;
-    // this.result = { ...data };
     this.options = { resolvers };
   }
 
-  resolve(data, context) {
-    if (isEmpty(this.options.resolvers)) {
-      return data;
+  resolve(data, context, callback) {
+    const { resolvers } = this.options;
+
+    if (isEmpty(resolvers)) {
+      return callback(data);
     }
 
-    const results = { ...data };
+    const propertyResolver = new PropertyResolver({
+      data,
+      context,
+      resolvers
+    });
 
-    const resolver = new PropertyResolver(result, resolvers);
+    const result = { ...data };
+    const keys = Object.keys(resolvers);
+    let done = 0
 
-    Object.entries(this.options.resolvers).forEach(([key, resolver]) => {
-      const resolved = resolver(data, context, results);
+    const handleDone = (key, value) => {
+      done++
+      if (value !== undefined) {
+        result[key] = value;
+      }
+      if (done === keys.length) {
+        callback(result);
+      }
+    }
+
+    keys.forEach((key) => {
+      const resolved = propertyResolver.resolve(key);
+      if (isPromise(resolved)) {
+        resolved.then((value) => handleDone(key, value));
+        return;
+      }
+      handleDone(key, resolved)
     });
   }
 }
 
-// function callbackPromises(maybePromise, callback) {
-//   if (isPromise(maybePromise)) {
-//     return maybePromise.then(callback);
-//   }
-
-//   return callback(maybePromise);
-// }
-
 class PropertyResolver {
-  constructor(result, any, resolvers: ResolverFunctions) {
-    // this.data = data;
-    // this.result = { ...data };
-    this.options = { resolvers };
+  options: {
+    data: Record<string, any>;
+    context: HookContext;
+    resolvers: ResolverFunctions;
+    cache: Record<string, Promise<any>>;
+    edges: Array<[string, string]>,
+    key: string | null;
   }
 
-  resolve(data) { }
+  constructor(options) {
+    this.options = {
+      cache: {},
+      edges: [],
+      key: null,
+      ...options
+    };
+  }
+
+  resolve(key) {
+    const {
+      data,
+      context,
+      resolvers,
+      cache,
+      edges,
+      key: parentKey
+    } = this.options;
+
+    const resolver = resolvers[key];
+
+    if (!resolver) {
+      return data[key];
+    }
+
+    const cached = cache[key];
+
+    if (cached) {
+      return cached;
+    }
+
+    if (parentKey) {
+      edges.push([parentKey, key]);
+      try {
+        toposort(edges);
+      } catch (error) {
+        throw new GeneralError(error.message);
+      }
+    }
+
+    const propertyResolver = new PropertyResolver({ ...this.options, key });
+
+    const resolved = resolver(data, context, propertyResolver);
+
+    cache[key] = resolved;
+
+    return resolved;
+  }
 }
